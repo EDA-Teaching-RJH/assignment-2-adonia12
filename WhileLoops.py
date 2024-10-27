2a640ee483f19d25fa2e10fe9efeca75b5990970
adonia12 <aa2574@kent.ac.uk>
2024-10-27 22:26:02 +0000

    part2

--- WhileLoops.py
+++ WhileLoops.py
@@ -6,7 +6,7 @@ while usernumber != guessnum:
     usernumber = int(input("Guess a number between 1 and 100   "))
     if usernumber < guessnum:
         print("Too low! Try again.")
-    elif usernumber > guessnum:
+    elif usernumber  > guessnum:
         print("Too high! Try again.")
     else:
         print("You've guessed the correct number.")
