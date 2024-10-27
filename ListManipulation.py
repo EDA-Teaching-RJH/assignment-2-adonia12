1e9c615d56b1a0c68ff86e222276a8dd45e1bcbb
adonia12 <aa2574@kent.ac.uk>
2024-10-27 22:29:48 +0000

    part3

--- ListManipulation.py
+++ ListManipulation.py
@@ -1,8 +1,8 @@
 ### Part Three -- your code goes here. 
 num_list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
-num_list.sort()
-num_list = [num for num in num_list if num != 1]
-num_list.extend([7,8])
+num_list.sort()      #sorts in ascending order  
+num_list = [num for num in num_list if num != 1]   #removing occurrences of number 1
+num_list.extend([7,8])        #adds number 7 and 8 to the end of the list 
 print(num_list)
 
 
